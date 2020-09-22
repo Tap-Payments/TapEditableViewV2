f7b297e80dfe633c7918beb926b224d2f70b4a50
O Rabie <o.rabie@tap.company>
2020-09-22 17:03:02 +0200

    Upgrade syntax to match the compiler in Xcode 12+

---
+++ TapEditableViewV2/TapEditableViewV2/TapEditableViewV2.h
@@ -0,0 +1,18 @@
+//
+//  TapEditableViewV2.h
+//  TapEditableViewV2
+//
+//  Created by Osama Rabie on 9/22/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TapEditableViewV2.
+FOUNDATION_EXPORT double TapEditableViewV2VersionNumber;
+
+//! Project version string for TapEditableViewV2.
+FOUNDATION_EXPORT const unsigned char TapEditableViewV2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TapEditableViewV2/PublicHeader.h>
+
+
